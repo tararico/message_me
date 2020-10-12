8ea215be459c10c675cf5a604fe4cb767e9972f9
tararico <emipu.uq@gmail.com>
2020-10-12 14:03:55 +0900

    user modelを作成しbcryptをインストールした

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([{ username: 'my' }, { password_digest: 'password' }])
+User.create([{ username: 'tkp' }, { password_digest: 'password' }])
+User.create([{ username: 'taitai' }, { password_digest: 'password' }])
+User.create([{ username: 'ojigi' }, { password_digest: 'password' }])
+User.create([{ username: 'yamepi' }, { password_digest: 'password' }])
+
